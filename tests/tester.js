d8920fa4cc1272e447ab51e6246b23df68a06842
Mark Gulyaev <duznet@gmail.com>
2013-09-20 04:15:26 +1100

    Add the simple tester

---
+++ tests/tester.js
@@ -0,0 +1,38 @@
+function runTests () {
+    for (var i = 0; i < tests.length; i++) {
+        var test = tests[i];
+        test.id = i;
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5000",
+            data: JSON.stringify(input),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data) {
+                test.got = JSON.parse(data);
+                check(test);
+            },
+            error: function () {
+                writeResult("failed", test, "Could not get the response!");
+            }
+        });
+    };
+};
+
+function check(test) {
+    var result = "";
+    for (var property in expected) {
+        if (got[property] !== expected[property]) {
+            result = "failed";
+            break;
+        }
+        result = "ok";
+    };
+    writeResult(result, test);
+}
+
+function writeResult(result, test, message = "") {
+    $('<div>').addClass(result)
+        .html("#" + test.id + " " + test.name + ": " + result + " " + message)
+        .appendTo($('#results'));
+}
