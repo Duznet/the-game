f8e1cb7d03d49d06a7df4f3673590c5682925069
Max Sporyshev <coockoombra@gmail.com>
2013-11-06 13:41:17 +1100

    Fix coffee compiling to work with nested directories.

--- compile_coffee.py
+++ compile_coffee.py
@@ -13,17 +13,22 @@ class CoffeeCompiler:
     def compile(self):
         if not os.path.exists(self.OUTPUT_DIR):
             os.mkdir(self.OUTPUT_DIR)
-        for root, dirnames, filenames in os.walk(self.dir):
+
+        for root, dirnames, filenames in os.walk("./"):
             for dirname in dirnames:
-                for filename in glob(os.path.join(self.dir, dirname, "*.coffee")):
+                for filename in glob(os.path.join(root, dirname, "*.coffee")):
                     file = open(filename, "r")
                     basename = os.path.basename(filename)
 
-                    folder = os.path.join(self.OUTPUT_DIR, dirname)
-                    if not os.path.exists(folder):
-                        os.mkdir(folder)
+                    filename = filename.replace(self.dir, self.OUTPUT_DIR)
+                    dirname = os.path.dirname(filename)
 
-                    output = open(os.path.join(folder, os.path.splitext(basename)[0] + ".js"), "w")
+                    if not os.path.exists(dirname):
+                        os.mkdir(dirname)
+
+                    print(filename)
+
+                    output = open(os.path.join(os.path.splitext(filename)[0] + ".js"), "w")
                     output.write(coffeescript.compile(file.read()))
 
 
